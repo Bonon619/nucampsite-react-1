734969d0dfbe9e04c92f656d1b6cef1e15fca5a2
Anh Nguyen <anhnguyen7496@gmail.com>
2023-02-13 10:04:34 -0600

    Header, Footer, Campsites

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./App.css";
+import CampsitesList from "./features/campsites/CampsitesList";
+import { CAMPSITES } from "./app/shared/CAMPSITES";
 
 function App() {
-  return <div className="App">Hello nucamp!</div>;
+  return (
+    <div className="App">
+      <Header />
+      <CampsitesList />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
